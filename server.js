f390c3d63a5102836088d131f1fd365873ad4d0a
Mohsin Ali <mohsin.ali.the.great@gmail.com>
2020-01-28 15:40:52 +0500

    Working Build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer')
 
 CATEGORIES = ['mediterranean', 'greek', 'middle+eastern']
 COORDINATES = '42.0493507,-87.6819763'
+ZOOM = 14
 
 async function browse() {
   const browser = await puppeteer.launch({headless: false, defaultViewport: null})
@@ -11,7 +12,7 @@ async function browse() {
     page.setDefaultNavigationTimeout(0)
     await page.goto(`https://www.google.com/maps/search/${category}+restaurant/@${COORDINATES},14z`, {timeout : 0, waitUntil : 'networkidle0'})
 
-    while (true) { //TODO set zoom and result end condition
+    while (true) {
       const count = await page.$$eval('.section-result[data-section-id^="or:"]', elements => elements.length)
 
       for (let i = 1; i <= count; i++) {
@@ -26,10 +27,16 @@ async function browse() {
         await page.waitForSelector(`.section-result`)
       }
 
-      const firstName = await page.$eval('[data-section-id^="or:"] [jstcache="124"]', element => element.textContent)
+      let nextDisabled = await page.evaluate(`document.querySelector('[aria-label=" Next page "]').disabled`)
+      if (nextDisabled) break
+      let zoom = Object.values(page.url().match(/,[0-9]+z/i))[0].slice(1,-1)
+      if (zoom < ZOOM) break
 
+      const firstName = await page.$eval('[data-section-id^="or:"] [jstcache="124"]', element => element.textContent)
+      
       await page.waitFor(1000)
       await page.click('[aria-label=" Next page "]')
+      console.log(`document.querySelector('[data-section-id^="or:"] [jstcache="124"]').innerText != '${firstName}'`)
       await page.waitForFunction(`document.querySelector('[data-section-id^="or:"] [jstcache="124"]').innerText != '${firstName}'`)
     }
 
