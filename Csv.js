8ea9c8a24518aef923957ea551a9ec267528f862
Mohsin Ali <mohsin.ali.the.great@gmail.com>
2020-02-11 12:06:37 +0500

    renaming Csv.js

---
+++ Csv.js
@@ -0,0 +1,39 @@
+const { Parser } = require('json2csv')
+const fs = require('fs')
+
+exports.Save = function (results) {
+    let dataFrame = []
+
+    var ToMilitaryTime = function (label) {
+        if (label == undefined) return undefined
+        else if (label.endsWith('AM'))
+          return label.startsWith('12') ? 0000 : parseInt(label.slice(0,-2)) * 100
+        else if (label.endsWith('PM'))
+            return label.startsWith('12') ? 1200 : parseInt(label.slice(0,-2)) * 100 + 1200
+    }
+
+    for (result of results) {
+        let row = {
+            name : result.name,
+            rating : result.rating, 
+            price : result.price,
+            address : result.address,
+            pluscode : result.pluscode,
+            phone : result.phone
+        }
+        for (openHour of result.openHours) {
+            row[openHour.day + '_closed'] = openHour.closed
+            row[openHour.day + '_open'] = ToMilitaryTime(openHour.open)
+            row[openHour.day + '_close'] = ToMilitaryTime(openHour.close)
+        }
+        for (popularity of result.popularity) {
+            const time = ToMilitaryTime(popularity.hour)
+            row[popularity.day + '_' + time] = popularity.popularity
+        }
+        dataFrame.push(row)
+    }
+
+    const parser = new Parser()
+    const csv = parser.parse(dataFrame)
+    fs.writeFile('Businesses.csv', csv, function (err) {if (err) throw err})
+}
